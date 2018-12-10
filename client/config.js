09bddf7ec66f2e8daf7b553be75ae9388ceea71a
aspengs <jmf@grayshirts.com.ar>
2018-12-10 12:52:55 -0300

    webpack

---
+++ client/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  clientPort: 3000,
+  serverPort: 5000,
+  cors: "localhost"
+};
---
+++ client/webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+    // new HtmlWebPackPlugin({
+    //   template: "./src/index.html",
+    //   filename: "./index.html"
+    // })
+  ],
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  }
+};
