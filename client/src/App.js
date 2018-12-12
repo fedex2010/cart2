97e3faf8447f4830bff9e4914f1019eaf43f05d9
Ignacio Andrada <ignacioandrada@nbgue12121.garbarino.com.ar>
2018-12-12 14:22:23 -0300

    Card

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Alert from './components/alert/alert';
+import Card from './components/product/card/card';
 
 import './App.scss';
 
@@ -20,6 +21,8 @@ class App extends Component {
       <Alert mensaje={"El producto seleccionado está agotado. Eliminalo para poder continuar."} tipo={"error"}/>
       <Alert mensaje={"Atención! No es posible comprar estos productos en el mismo carrito.Te sugerimos comprarlos por separado así podemos ofrecerte más opciones de entrega."} tipo={"error"}/>
       <Alert mensaje={"Ocurrio un error. Intente nuevamente más tarde."} tipo={"error"}/>
+
+      <Card tipo={"success"}/>
 
    </div>
   );
