b47c6a2f1587bf84615ab1d196fded9cb97c581d
aspengs <jmf@grayshirts.com.ar>
2018-12-12 16:59:03 -0300

    reducers

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,6 @@
-/*
- * src/store.js
- * No initialState
-*/
-
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer';
-export default function configureStore(initialState={}) {
- return createStore(
-  rootReducer,
-   applyMiddleware(thunk)
- );
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/rootReducer";
+export default function configureStore(initialState = {}) {
+  return createStore(rootReducer, applyMiddleware(thunk));
 }
