bee6b50ecc42eb54d4c68e997d23315652ec8370
Ignacio Andrada <ignacioandrada@nbgue12121.garbarino.com.ar>
2018-12-12 10:11:44 -0300

    ComponentsAlert

--- client/src/App.js
+++ client/src/App.js
@@ -13,10 +13,20 @@ class App extends Component {
  
 
 
+
   render() {
   return (
    <div className="App">
-      <Alert/>
+      <Alert mensaje={"¡Buenas noticias! Tenés un descuento especial por producto combinado y bonificación."} tipo={"success"}/>
+      <Alert mensaje={"¡Buenas noticias! Tenés un descuento especial por bonificación."} tipo={"success"}/>
+      <Alert mensaje={"¡Buenas noticias! Tenés un descuento especial por producto combinado."} tipo={"success"}/>
+      <Alert mensaje={"¡Buenas noticias! Tenes un cupón de descuento aplicado."} tipo={"success"}/>
+
+
+      <Alert mensaje={"El producto seleccionado está agotado. Eliminalo para poder continuar."} tipo={"error"}/>
+      <Alert mensaje={"Atención! No es posible comprar estos productos en el mismo carrito.Te sugerimos comprarlos por separado así podemos ofrecerte más opciones de entrega."} tipo={"error"}/>
+      <Alert mensaje={"Ocurrio un error. Intente nuevamente más tarde."} tipo={"error"}/>
+
    </div>
   );
  }
--- client/src/components/alert/alert.js
+++ client/src/components/alert/alert.js
@@ -8,12 +8,35 @@ import { connect } from 'react-redux';
 
 class Alert extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      mensaje: this.props.mensaje,
+      tipo: this.props.tipo
+    };
+  }
+
+
+
   render() {
-    return (
-    <div className="alertComponet">
-        
-    </div>
-    );
+
+    if(this.state.tipo == "success"){
+      return (
+        <div className="feedback feedback-error feedback-dismissible">
+          <button type="button" className="feedback--btn-close"></button>
+          {this.state.mensaje}
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="feedback feedback-error feedback-dismissible">
+          <button type="button" className="feedback--btn-close"></button>
+          {this.state.mensaje}
+        </div>
+      );
+    }
+    
     }
 }
 export default Alert;
