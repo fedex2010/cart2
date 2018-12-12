6977443118ccf347376f0515ff24e8f624878613
Ignacio Andrada <ignacioandrada@nbgue12121.garbarino.com.ar>
2018-12-12 09:09:12 -0300

    Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,90 +1,26 @@
-/*
- src/App.js
-*/
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import logo from './logo.svg';
+
+
+import Alert from './components/alert/alert';
+
 import './App.scss';
-import { simpleAction } from './actions/simpleAction'
 
-const mapStateToProps = state => ({
-  ...state
- })
 
- const mapDispatchToProps = dispatch => ({
-  simpleAction: () => dispatch(simpleAction())
- })
 
 class App extends Component {
  
-  simpleAction = (event) => {
-    this.props.simpleAction();
-  }
+
 
   render() {
   return (
    <div className="App">
-    <header className="App-header">
-     <img src={logo} className="App-logo" alt="logo" />
-     <h1 className="App-title">Welcome to React</h1>
-     <button onClick={this.simpleAction}>Test redux action</button>
-     <pre>
- {
-  JSON.stringify(this.props)
- }
-</pre>
-    </header>
-    <p className="App-intro">
-     To get started, edit <code>src/App.js</code> and save to reload
-    </p>
+      <Alert/>
    </div>
   );
  }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-
-/*import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
-
-class App extends Component {
-  
-  state = { users: [] }
-
-  componentDidMount() {
-    fetch('/users')
-    .then( res => res.json())
-    .then( users => this.setState({ users}))
-  }
-  render() {
-    return (
-      <div className="App">
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <h1>Users</h1>
-        <ul>
-          {this.state.users.map(user =>
-            <li key={user.id}>{user.username}</li>  
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
-
 export default App;
-*/
+
+
---
+++ client/src/components/alert/alert.js
@@ -0,0 +1,21 @@
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+
+
+
+class Alert extends Component {
+
+  render() {
+    return (
+    <div className="alertComponet">
+        
+    </div>
+    );
+    }
+}
+export default Alert;
+
+
