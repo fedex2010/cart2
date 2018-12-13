b720bc1dfc8a8dba3d76d2ceecbd236f4a7984a5
aspengs <jmf@grayshirts.com.ar>
2018-12-13 17:59:24 -0300

    saved components

---
+++ client/src/components/Test/Test.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './Test.scss';
+
+class Test extends PureComponent {
+ 
+  render() {
+  return (
+   <div className="Test">
+      <p>Component Test</p>
+   </div>
+  );
+ }
+}
+export default Test;
