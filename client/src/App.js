314d907f5b9af9d0571cc856afc264a21dbd5a6a
Ignacio Andrada <ignacioandrada@nbgue12121.garbarino.com.ar>
2018-12-13 11:35:56 -0300

    VariablesCartUi

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,14 @@ import Card from './components/product/card/card';
 import Summary from './components/summary/summary';
 import Carousel from './components/carousel/carousel';
 
-import Test from './components/Test/Test';
+//import Test from './components/Test/Test';
 
 class App extends Component {
  
   render() {
   return (
 
-   <div className="gb-wrapper">
+   <div className="gb-wrapper container-fluid">
       <div className="row">
          <div className="alert-message-gbChk col-md-12">
              <Alert mensaje={"¡Buenas noticias! Tenés un descuento especial por producto combinado y bonificación."} tipo={"success"}/>
@@ -29,10 +29,10 @@ class App extends Component {
              <Alert mensaje={"Atención! No es posible comprar estos productos en el mismo carrito.Te sugerimos comprarlos por separado así podemos ofrecerte más opciones de entrega."} tipo={"error"}/>
              <Alert mensaje={"Ocurrio un error. Intente nuevamente más tarde."} tipo={"error"}/>
          </div>
-         <Test />
+        {/* <Test />
        <button onClick={this.simpleAction} className='jerry'>Test redux action</button>
-         
-         <div className="product-summary-gbChk">
+        */}
+         <div className="row product-summary-gbChk">
             <div className="product-gbChk col-md-8">
                <Card tipo={"success"}/>
             </div>
--- client/src/components/summary/summary.js
+++ client/src/components/summary/summary.js
@@ -8,20 +8,21 @@ class Summary extends Component {
   render() {
   return (
    <div className="summary">
-        <div className="card">
-            Resumen de compra
-            Subtotal $21.296
-            Descuento especial - $600
-            Total $20.696,10
-            DESCUENTOS Y CUPONES
+        <div className="summary-fixed">
+            <div className="card">
+                Resumen de compra
+                Subtotal $21.296
+                Descuento especial - $600
+                Total $20.696,10
+                DESCUENTOS Y CUPONES
 
-            Descuento especial
-            Tengo cupón de descuento
+                Descuento especial
+                Tengo cupón de descuento
+            </div>
+
+            <a href="#">COMPRAR MÁS PRODUCTOS</a>
+            <input type="button" value="Continuar"/>
         </div>
-
-        <a href="#">COMPRAR MÁS PRODUCTOS</a>
-        <input type="button" value="Continuar"/>
-
    </div>
   );
  }
