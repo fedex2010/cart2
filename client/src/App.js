6a8fd33a7a0c29c5985e76869d25e8d7132f70c2
Leandro Gedeon <lgedeon@lgedeon.garbarino.com.ar>
2018-12-13 08:38:47 -0300

    add garba-ui

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import logo from './logo.svg';
-import './App.scss';
 import { simpleAction } from './actions/simpleAction'
+import './App.scss';
+import Test from './components/Test/Test'
 
 const mapStateToProps = state => ({
   ...state
@@ -26,8 +27,9 @@ class App extends Component {
    <div className="App">
     <header className="App-header">
      <img src={logo} className="App-logo" alt="logo" />
+     <Test />
      <h1 className="App-title">Welcome to React</h1>
-     <button onClick={this.simpleAction}>Test redux action</button>
+     <button onClick={this.simpleAction} className='jerry'>Test redux action</button>
      <pre>
  {
   JSON.stringify(this.props)
@@ -42,49 +44,3 @@ class App extends Component {
  }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-
-/*import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
-
-class App extends Component {
-  
-  state = { users: [] }
-
-  componentDidMount() {
-    fetch('/users')
-    .then( res => res.json())
-    .then( users => this.setState({ users}))
-  }
-  render() {
-    return (
-      <div className="App">
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <h1>Users</h1>
-        <ul>
-          {this.state.users.map(user =>
-            <li key={user.id}>{user.username}</li>  
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
-
-export default App;
-*/
