223e87591000c210556d8d3297540f8e49fc60cb
Ignacio Andrada <ignacioandrada@nbgue12121.garbarino.com.ar>
2018-12-13 10:11:43 -0300

    BoostrapGrid

--- client/src/App.js
+++ client/src/App.js
@@ -19,31 +19,28 @@ class App extends Component {
   render() {
   return (
 
-   <div className="container-gbChk">
-      <div className="gbChk-row">
-         <div className="alert-message-gbChk gbChk-12">
+   <div className="gb-wrapper">
+      <div className="row">
+         <div className="alert-message-gbChk col-md-12">
              <Alert mensaje={"¡Buenas noticias! Tenés un descuento especial por producto combinado y bonificación."} tipo={"success"}/>
              <Alert mensaje={"¡Buenas noticias! Tenés un descuento especial por bonificación."} tipo={"success"}/>
              <Alert mensaje={"¡Buenas noticias! Tenés un descuento especial por producto combinado."} tipo={"success"}/>
              <Alert mensaje={"¡Buenas noticias! Tenes un cupón de descuento aplicado."} tipo={"success"}/>
-
-
              <Alert mensaje={"El producto seleccionado está agotado. Eliminalo para poder continuar."} tipo={"error"}/>
              <Alert mensaje={"Atención! No es posible comprar estos productos en el mismo carrito.Te sugerimos comprarlos por separado así podemos ofrecerte más opciones de entrega."} tipo={"error"}/>
              <Alert mensaje={"Ocurrio un error. Intente nuevamente más tarde."} tipo={"error"}/>
-      </div>
+         </div>
        <button onClick={this.simpleAction} className='jerry'>Test redux action</button>
           <Test/>
          <div className="product-summary-gbChk">
-            <div className="product-gbChk gbChk-8">
+            <div className="product-gbChk col-md-8">
                <Card tipo={"success"}/>
             </div>
-            <div className="summary-gbChk gbChk-4">
+            <div className="summary-gbChk col-md-4">
                <Summary tipo={"success"}/>
             </div>
          </div>
-     
-         <div className="carousel-gbChk gbChk-12">
+         <div className="carousel-gbChk col-md-12">
          <Carousel tipo={"success"}/>
          </div>
       </div>
