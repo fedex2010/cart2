e30e18c10203b6314ff43207c1985218709a2abd
Ignacio Andrada <ignacioandrada@nbgue12121.garbarino.com.ar>
2018-12-17 16:45:28 -0300

    RadioButtonChecked

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import "./App.scss";
 
 
-import Alert from "./components/alert/alert";
+//import Alert from "./components/alert/alert";
 import Product from "./components/product/product";
 import Summary from "./components/summary/summary";
 import Carousel from "./components/carousel/carousel";
@@ -34,17 +34,17 @@ class App extends Component {
             <h1 className="cart-title">Mi Carrito</h1>
             <div className="row">
               <div className="alert-message-gbChk col-md-12">
-                {/* mensajes de error */}
+                {/* mensajes de error 
                 <Alert mensaje={" El producto seleccionado está agotado. Eliminalo para poder continuar."} tipo={"error"}/>
                 <Alert mensaje={"Atención!.No es posible comprar estos productos en el mismo carrito. Te sugerimos comprarlos por separado así podemos ofrecerte más opciones de entrega."} tipo={"error"}/>
                 <Alert mensaje={"Ocurrio un error. Intente nuevamente más tarde."} tipo={"error"}/>
-                {/* mensajes de success */}
+                mensajes de success 
                 <Alert mensaje={"¡Buenas noticias! Tenés un descuento especial por producto combinado y bonificación."} tipo={"success"}/>
                 <Alert mensaje={"¡Buenas noticias! Tenés un descuento especial por bonificación."} tipo={"success"}/>
                 <Alert mensaje={"¡Buenas noticias! Tenés un descuento especial por producto combinado."} tipo={"success"}/>
                 <Alert mensaje={"¡Buenas noticias! Tenes un cupón de descuento aplicado."} tipo={"success"}/>
                 
-                {/* mensajes de info (no se usa actualmente) */}
+                 mensajes de info (no se usa actualmente)
                 <div className="feedback feedback-info feedback-dismissible">
                   <button type="button" className="feedback--btn-close" />
                   <p>
@@ -55,7 +55,7 @@ class App extends Component {
                     .
                   </p>
                 </div>
-
+                */}
               </div>  
             </div> 
             <div className="product-summary-gbChk row">
