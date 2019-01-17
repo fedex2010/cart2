c659bab71bbd963b6ab5e1495c0d1fcac78214ed
Ignacio Andrada <ignacioandrada@nbgue12121.garbarino.com.ar>
2019-01-17 12:18:15 -0300

    link-to-button in ProductWarranty

--- client/src/components/Product/ProductWarranty.js
+++ client/src/components/Product/ProductWarranty.js
@@ -74,8 +74,8 @@ class ProductWarranty extends Component{
                         {
                             liWarranty.map((item, i)=>{
                                 checked = (this.props.warranty_id !== "DEFAULT_FACTORY" && this.props.warranty_id === item.id)?true:false;
-                                return (<li key={i} ><label><input type="checkbox" value={item.id} checked={ checked } onChange={this._onSelectOption.bind(this,item.id,product_id)} /><a href="#" data-toggle="modal"
-                                data-target="#warranty-modal" onClick={this._showModal.bind(this, this.props.current, item.id)} >{item.prod} meses</a> de protección por{" "}<strong>${item.price}</strong> ó 12 cuotas de <strong>${Math.ceil(item.installment_price)}</strong></label></li>)
+                                return (<li key={i} ><label><input type="checkbox" value={item.id} checked={ checked } onChange={this._onSelectOption.bind(this,item.id,product_id)} /><button className="link-to-button" data-toggle="modal"
+                                data-target="#warranty-modal" onClick={this._showModal.bind(this, this.props.current, item.id)} >{item.prod} meses</button> de protección por{" "}<strong>${item.price}</strong> ó 12 cuotas de <strong>${Math.ceil(item.installment_price)}</strong></label></li>)
 
                             })
                         }
