28158be92dfc6614f6aeaf446ffc7cb811c2572b
Federico Ferreyra <federico@grayshirts.com.ar>
2019-01-25 11:17:55 -0300

    isolate cart routes

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -3,21 +3,27 @@ var express = require("express"),
     controllers = require("../controllers"),
     bodyParser = require("body-parser"),
     urlencodedParser = bodyParser.urlencoded({ extended: false }),
-    jsonParser = bodyParser.json(),
-    cartRoutes = require('./cart');
+    jsonParser = bodyParser.json()
 
-testMiddleware = ( req , res ,next) => {
-    console.log("HOLA")
-    console.log("HOLA")
-    next()
-}
+router.get("/normandia", ( req , res) => controllers.normandia.getTemplate( req , res ))
+router.get("/carousel",  ( req , res) => controllers.cart.getCarousel( req , res ));
+router.get("/summary", ( req , res) => controllers.cart.summary( req , res ));
+router.get("/c_:cartId/warranty/:productId", ( req , res) => controllers.cart.warrantyMobile( req , res ));
+router.get("/:cartId", ( req , res) => controllers.cart.getCart( req , res ));
 
-router.get("/health", ( req , res) => { res.status(200).send("OK");});
-router.get("/",( req , res ) => controllers.cart.renderApp( req , res ))
+router.post("/fake_product_view/:productId", urlencodedParser, ( req , res) => controllers.cart.fake_product(req , res));
+router.post("", urlencodedParser, ( req , res) => controllers.cart.addProduct(req , res));
+router.post("/:cartId/cupon", jsonParser, ( req , res) => controllers.cart.setCoupon(req , res));
+router.post("/c_:cartId/aaPlus", jsonParser, ( req , res) => controllers.cart.setAAPlus(req , res));
+router.post("/setWarranty", jsonParser, ( req , res) => controllers.cart.setWarranty( req , res));
+router.post("/vendedor", urlencodedParser, ( req , res) =>  controllers.cart.sellerLoginAction( req , res));
 
-router.get("/cart", testMiddleware ,cartRoutes )
+router.put("/:cartId", urlencodedParser, ( req , res) =>  controllers.cart.editProduct( req , res));
+
+router.delete("/:productId", urlencodedParser, (req , res) => controllers.cart.deleteProduct( req , res));
+router.delete( "/c_:cartId/cupon/:couponCode", ( req , res) => controllers.cart.deleteCoupon( req , res));
+router.delete("/c_:cartId/aaPlus", ( req , res) => controllers.cart.deleteAAPlus( req , res));
   
-
 module.exports = router;
 /*
 TODO Estas son las rutas actuales del carrito a replicar
