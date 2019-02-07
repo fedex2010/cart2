906703c07df1ad1517113a3d87b92d17751ee908
aspengs <jmf@grayshirts.com.ar>
2019-02-07 13:41:56 -0300

    fix

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-
 import rootReducer from "./Reducers/RootReducer";
-
 import { composeWithDevTools } from "redux-devtools-extension";
 import { createLogger } from "redux-logger";
 
