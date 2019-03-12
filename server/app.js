169fb213e65559afb7f771911a314a90d7307bcb
aspengs <jmf@grayshirts.com.ar>
2019-03-12 11:31:35 -0300

    redirect /carrito on post

--- server/app.js
+++ server/app.js
@@ -8,28 +8,24 @@ const express = require("express"),
       errorService = require('./services/error_service'),
       cookieParser   = require('cookie-parser'),
       indexRouter = require("./routes/index"),
-      cartRouter = require("./routes/cart");
+      cartRouter = require("./routes/cart"),
+      carrito = require("./routes/carrito");
 
 let app = express();
 
 // view engine setupS
-// app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "jade");
-
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
-
 //app.use(parallel([cookie]));
 
 app.use("/", indexRouter);
-//app.get("/api/",( req , res ) => controllers.cart.renderApp( req , res ))
 app.get("/api/health", ( req , res) => { res.status(200).send("OK");});
-
 app.use("/api/cart", sessionMiddleware , cartRouter);
 app.use("/reactcart/api/cart", sessionMiddleware , cartRouter);
+app.use("/carrito",sessionMiddleware, carrito);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
