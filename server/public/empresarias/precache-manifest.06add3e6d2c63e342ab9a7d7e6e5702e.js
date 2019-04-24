0ac6d4ee427a59c27cc19f038bc5140bbd174bd4
aspengs <jmf@grayshirts.com.ar>
2019-04-24 00:53:32 -0300

    test 2

---
+++ server/public/empresarias/precache-manifest.06add3e6d2c63e342ab9a7d7e6e5702e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5e327f3117b300401f62dfdf07bdc99",
+    "url": "//dj4i04i24axgu.cloudfront.net/cart-ui/empresarias/static/media/gui-icons.c5e327f3.eot"
+  },
+  {
+    "revision": "bcc21aaedb7e977499565bbc73c47674",
+    "url": "//dj4i04i24axgu.cloudfront.net/cart-ui/empresarias/static/media/gui-icons.bcc21aae.woff"
+  },
+  {
+    "revision": "354db0f0be850b2dab51329bad2b14a1",
+    "url": "//dj4i04i24axgu.cloudfront.net/cart-ui/empresarias/static/media/gui-icons.354db0f0.ttf"
+  },
+  {
+    "revision": "327a39e831bc6c2804eb5ec9ea83ad6b",
+    "url": "//dj4i04i24axgu.cloudfront.net/cart-ui/empresarias/static/media/gui-icons.327a39e8.svg"
+  },
+  {
+    "revision": "466a5adcc0213a2002d020c661d5655c",
+    "url": "//dj4i04i24axgu.cloudfront.net/cart-ui/empresarias/static/media/empty.466a5adc.png"
+  },
+  {
+    "revision": "63d947dcc94dd817d3b8",
+    "url": "//dj4i04i24axgu.cloudfront.net/cart-ui/empresarias/static/js/runtime~main.63d947dc.js"
+  },
+  {
+    "revision": "473905affbe78a4bae5c",
+    "url": "//dj4i04i24axgu.cloudfront.net/cart-ui/empresarias/static/js/main.473905af.chunk.js"
+  },
+  {
+    "revision": "83f02b2f311b08179b82",
+    "url": "//dj4i04i24axgu.cloudfront.net/cart-ui/empresarias/static/js/1.83f02b2f.chunk.js"
+  },
+  {
+    "revision": "473905affbe78a4bae5c",
+    "url": "//dj4i04i24axgu.cloudfront.net/cart-ui/empresarias/static/css/main.d6fe8a20.chunk.css"
+  },
+  {
+    "revision": "3c802edf3c07e4782ee3fda8b62dc2e7",
+    "url": "//dj4i04i24axgu.cloudfront.net/cart-ui/empresarias/index.html"
+  }
+];
